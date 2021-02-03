c75f50ee0dad806638f2fec671389693476d9f7a
refm.130995 <refm.130995@gmail.com>
2021-02-03 16:20:47 -0400

    Enviroments variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,30 @@
 export const environment = {
-  production: true
+  production: false,
+  URL_API_BASE: 'https://localhost:8100/api',
+  URL_IMG_BASE: 'https://localhost:8100',
+  async getToken() {
+    return (
+      JSON.parse(localStorage.getItem(this.storageKeys.token_type)) +
+      ' ' +
+      JSON.parse(localStorage.getItem(this.storageKeys.access_token) || '')
+    );
+  },
+
+  getUser() {
+    return JSON.parse(localStorage.getItem(this.storageKeys.user));
+  },
+
+  get storageKeys() {
+    return {
+      user: 'user',
+      token_type: 'token_type',
+      access_token: 'access_token',
+    };
+  },
+
+  get DefaultMessages() {
+    return {
+      error: 'Ocurrió un error inesperado. Pongase en contacto con nosotros.',
+    };
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,30 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
+  production: false,
+  URL_API_BASE: 'https://localhost:8100/api',
+  URL_IMG_BASE: 'https://localhost:8100',
+  async getToken() {
+    return (
+      JSON.parse(localStorage.getItem(this.storageKeys.token_type)) +
+      ' ' +
+      JSON.parse(localStorage.getItem(this.storageKeys.access_token) || '')
+    );
+  },
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  getUser() {
+    return JSON.parse(localStorage.getItem(this.storageKeys.user));
+  },
+
+  get storageKeys() {
+    return {
+      user: 'user',
+      token_type: 'token_type',
+      access_token: 'access_token',
+    };
+  },
+
+  get DefaultMessages() {
+    return {
+      error: 'Ocurrió un error inesperado. Pongase en contacto con nosotros.',
+    };
+  },
+};
