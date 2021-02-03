2b6f8035aa88531a245a8cf072001fd94fa864b9
refm.130995 <refm.130995@gmail.com>
2021-02-03 15:02:52 -0400

    Configuracion de herramientas de maquetado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true,
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    DashboardComponent,
+    FooterComponent,
   ],
   imports: [
+    TooltipModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PerfectScrollbarModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    JwBootstrapSwitchNg2Module,
+    NgxIntlTelInputModule,
+    NgbModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
