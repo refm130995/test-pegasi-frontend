8fde0efc817962149590da7186de077f0bf1adde
refm.130995 <refm.130995@gmail.com>
2021-02-03 15:55:00 -0400

    Se maqueto la vista de login

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
   }
 
+  get getForm() {
+    return this.form.controls;
+  }
+
+  async onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+  }
 }
