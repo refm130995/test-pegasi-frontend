8f1cefd0877dd21e31a5cb0c6926918d480e6849
refm.130995 <refm.130995@gmail.com>
2021-02-03 15:07:13 -0400

    Creacion y enrutamiento de paginas iniciales a trabajar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: SignInComponent,
+    //canActivate: [GuardGuard],
+  },
+  {
+    path: 'signUp',
+    component: SignUpComponent,
+    //canActivate: [GuardGuard],
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./pages/user/user.module').then((m) => m.UserModule),
+  },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
