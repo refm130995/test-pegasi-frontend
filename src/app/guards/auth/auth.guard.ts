552d2de2da0c685852eda152fb4497104e28e5c2
refm.130995 <refm.130995@gmail.com>
2021-02-03 16:36:25 -0400

    Se implementaron login guards y api service con httpClient

---
+++ src/app/guards/auth/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(public auth: AuthService, public router: Router) {}
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigateByUrl('');
+      return false;
+    }
+  }
+}
---
+++ src/app/guards/guard.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class GuardGuard implements CanActivate {
+  constructor(public auth: AuthService, public router: Router) {}
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.auth.isAuthenticated()) {
+      this.router.navigateByUrl('user/dashboard');
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
